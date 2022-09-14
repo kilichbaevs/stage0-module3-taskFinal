8fcd9d9d61b5f4f1a28d64f4ff4c58e11e7ef4d3
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-14 11:55:38 +0500

    push

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.print(number % 10);
+        System.out.print((number % 100) / 10);
+        System.out.print((number / 100));
     }
 }
